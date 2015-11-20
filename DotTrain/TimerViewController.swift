2036540efc5597ed4a3ff45563fae7b93bf1c744
Ashwin Kumar <theshwin@yahoo.com>
2015-11-19 19:47:41 -0800

    added timer

--- DotTrain/TimerViewController.swift
+++ DotTrain/TimerViewController.swift
@@ -1,22 +1,27 @@
 //
-//  ViewController.swift
+//  TimerViewController.swift
 //  DotTrain
 //
-//  Created by Christian on 11/17/15.
+//  Created by Ashwin Kumar on 11/19/15.
 //  Copyright © 2015 Masa. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
 
+class TimerViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         
+        NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "animateOtherView", userInfo: nil, repeats: false)
     }
     
-
+    func animateOtherView() {
+        performSegueWithIdentifier("timerSegue", sender: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,10 +32,9 @@ class ViewController: UIViewController {
     }
     
     
-
     
-
-
-
+    
+    
+    
+    
 }
-
